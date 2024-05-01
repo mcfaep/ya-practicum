f1610cf79a249b257ad8579597479bd13dfa3809
mcfaep <167646411+mcfaep@users.noreply.github.com>
2024-05-01 21:47:45 +0500

    fix(controllers/games): added functions

--- controllers/games.js
+++ controllers/games.js
@@ -41,23 +41,15 @@ const sendAllGames = async (req, res) => {
     res.send(req.games);
 }
 
-const deleteGame = async (req, res) => {
-    const id = Number(req.params.id);
-    req.game = req.games.find((item) => item.id === id);
-
-    const index = req.games.findIndex((item) => item.id === req.game.id);
-    req.games.splice(index, 1);
-
-    await writeData('./data/games.json', req.games);
-
+const sendUpdatedGames = (req, res) => {
     res.send({
         games: req.games,
-        updated: req.game
+        updated: req.updatedObject
     });
-}
+};
 
 module.exports = {
     sendAllGames,
-    deleteGame,
     addGameController,
+    sendUpdatedGames,
 }
